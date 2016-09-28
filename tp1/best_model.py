747988b05f623cc0bec95face1bc5b7a10aa88c5
Martin Caravario <mcaravario@dc.uba.ar>
2016-09-27 23:43:12 -0300

    Modifico los archivos de entrenamiento de modelos para la competencia

--- tp1/best_model.py
+++ tp1/best_model.py
@@ -14,18 +14,20 @@ print "Cargando data frame..."
 ham_train = json.load(open('./data/ham_dev.json'))
 spam_train = json.load(open('./data/spam_dev.json'))
 
+#ham_train = ham_train[:len(ham_train)/2]
+#spam_train = spam_train[:len(spam_train)/2]
 
 
 X = ham_train+spam_train
 y = [0 for _ in range(len(ham_train))]+[1 for _ in range(len(spam_train))]
 
-cfl = Pipeline([
-	('extraction',			TfidfVectorizer(max_features=100, stop_words="english", lowercase=False)),
-	('selection',    		SelectKBest(k=50)),
+clf = Pipeline([
+	('extraction',			TfidfVectorizer(max_features=1000, stop_words="english", lowercase=False)),
+	('selection',    		SelectKBest(k=100)),
 	('classifier', 			RandomForestClassifier(max_features="sqrt",n_estimators=1,criterion="entropy"))])
 
 print "Entrenando"
-cfl.fit(X,y)
+clf.fit(X,y)
 print "Guardando"
 
 fout = open('mejor_modelo.pickle','w')
--- tp1/predict.py
+++ tp1/predict.py
@@ -1,14 +1,10 @@
+#!/usr/bin/python
 import pickle
 import sys
 import json
 
-args = sys.argv
-if len(args) != 2:
-	print "Falta filename de conjunto a testear"
-	sys.exit()	
-test_set = args[1]
-clf = pickle.load(open("mejor_modelo.pickle"))
-X = json.load(open('test.json'))
+clf = pickle.load( open('mejor_modelo.pickle') )
+X = json.load(open('./data/spam_dev.json'))
 predicted = clf.predict(X)
 for y in predicted:
 	if y == 1:
